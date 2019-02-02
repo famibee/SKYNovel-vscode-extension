def931f735f792c211c32be3c0c877613e0e547e
famibee <famibee@gmail.com>
2019-02-03 06:23:07 +0900

    v0.0.3：fn属性やlabel属性の値に下線を引くように

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,63 @@
 import * as vscode from 'vscode';
 
+interface DecChars {
+	aRange		: vscode.Range[];
+	decorator	: vscode.TextEditorDecorationType;
+}
+let decChars: DecChars = {
+	aRange: [],
+	decorator: vscode.window.createTextEditorDecorationType({})
+};
+let timeout: NodeJS.Timer | null = null;
+
 // ロード時に一度だけ呼ばれる
 export function activate(context: vscode.ExtensionContext) {
+	let edActive = vscode.window.activeTextEditor;
+	if (edActive) trgUpdDeco();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-//console.log('Congratulations, your extension "skynovel" is now active!');
+	vscode.window.onDidChangeActiveTextEditor(ed=> {
+		edActive = ed;
+		if (ed) trgUpdDeco();
+	}, null, context.subscriptions);
 
-	let disposable = vscode.commands.registerCommand('extension.SKYNovel', ()=> {
-		// コマンドが実行されるたびに、extension.SKYNovelが呼ばれ、この関数のみ実行される
-		vscode.window.showInformationMessage('Hello SKYNovel World!');
-	});
+	vscode.workspace.onDidChangeTextDocument(event=> {
+		if (edActive && event.document === edActive.document) trgUpdDeco();
+	}, null, context.subscriptions);
 
-	context.subscriptions.push(disposable);
+
+	function trgUpdDeco() {
+		if (timeout) clearTimeout(timeout);
+		timeout = setTimeout(updDeco, 500);
+	}
+	function updDeco() {
+		if (! edActive) return;
+		const src = edActive.document.getText();
+
+		vscode.window.setStatusBarMessage('');
+		decChars.decorator.dispose();
+		decChars = {
+			aRange: [],
+			decorator: vscode.window.createTextEditorDecorationType({
+				'light': {
+					'textDecoration': 'underline',
+				},
+				'dark': {
+					'textDecoration': 'underline',
+				}
+			})
+		}
+
+		const regex = new RegExp('\\s(fn|label)\\=([^\\]\\s]+)', 'g');
+		let m;
+		while (m = regex.exec(src)) {
+			const lenVar = m[1].length;
+			decChars.aRange.push(new vscode.Range(
+				edActive.document.positionAt(m.index +lenVar+2),
+				edActive.document.positionAt(m.index +lenVar+2 + m[2].length)
+			));
+		}
+		edActive.setDecorations(decChars.decorator, decChars.aRange);
+	}
 }
 
 // 拡張機能が非アクティブ化されたときに、実行
