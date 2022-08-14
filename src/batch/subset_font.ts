72db6c3405b5f20690f000b63b48940bf9f562a8
famibee <famibee@gmail.com>
2022-08-14 15:19:11 +0900

    v3.28.0：woff2 変換に失敗しても woff、ttf でリトライするように

--- src/batch/subset_font.ts
+++ src/batch/subset_font.ts
@@ -15,19 +15,35 @@ import {userInfo} from 'os';
 import {extname} from 'path';
 
 
-const oLog: {[nm: string]: {inp: string, out: string, iSize: number, oSize: number, err: string}} = {};
+type LOG = {inp: string, out: string, iSize: number, oSize: number, err: string};
+const oLog: {[nm: string]: LOG} = {};
 const log_exit = (exit_code = -1)=> {
 	writeJsonSync(__filename +'on', oLog, {encoding: 'utf8'});
 	if (exit_code > -1) process.exit(exit_code);
 }
 
-const fnc: (inp: string, out: string, str?: string)=> Promise<void> = minify
-	? async (inp, out, str)=> {
-		const bufIn = await readFile(inp);
-		const bufOut = await subsetFont(bufIn, str, {targetFormat: 'woff2',});
-		await outputFile(out, bufOut);
+const fnc: (log: LOG, str: string)=> Promise<void> = minify
+	? async (log, str)=> {
+		try {
+			const bufIn = await readFile(log.inp);
+
+			// woff2 変換に失敗しても woff なら成功する場合があるので、リトライ
+			const a = ['woff2','woff','sfnt'];
+			const len = a.length;
+			for (let i=0; i<len; ++i) {
+				try {
+					const bufOut = await subsetFont(bufIn, str, {targetFormat: a[i]});
+					log.out += ['woff2','woff','ttf'][i];
+					await outputFile(log.out, bufOut);
+					break;
+				} catch {
+					log.err += `【${a[i]} 変換失敗】\n`;
+					continue;
+				}
+			}
+		} catch (e) {log.err += e.message +'\n';}
 	}
-	: (inp, out)=> ensureLink(inp, out);
+	: log=> ensureLink(log.inp, log.out);
 
 const o = require('./font.json');
 const a = [];
@@ -46,28 +62,27 @@ for (const nm in o) {
 	.replace('::PATH_PRJ_FONTS::', PATH_PRJ_FONTS)
 	.replace('::PATH_USER_FONTS::', PATH_USER_FONTS)
 	.replace('::PATH_OS_FONTS::', PATH_OS_FONTS);
-	const out = `doc/prj/script/${nm}${minify ?'.woff2' :extname(inp)}`;
-	oLog[nm] = {inp, out, iSize: 1, oSize: 1, err: ''};
+	const out = `doc/prj/script/${nm}${minify ?'.' :extname(inp)}`;
+	const log = oLog[nm] = {inp, out, iSize: 1, oSize: 1, err: ''};
 	if (! existsSync(inp)) {
-		oLog[nm].err = `変換失敗です。入力ファイル ${o[nm].inp.slice(20)} が存在するか確認してください`;
+		log.err = `変換失敗です。入力ファイル ${o[nm].inp.slice(20)} が存在するか確認してください`;
 		continue;
 	}
-	a.push(fnc(inp, out, o[nm].txt));
+	a.push(fnc(log, o[nm].txt));
 }
 Promise.allSettled(a)
 .then(()=> {
 	for (const nm in oLog) {
-		const q = oLog[nm];
-		const inp = q.inp;
-		const out = q.out;
-		q.inp = o[nm].inp;	// プライベートな環境値を塗りつぶす
-	//	q.out = // これは存在しない
+		const log = oLog[nm];
+		const {inp, out} = log;
+		log.inp = o[nm].inp;	// プライベートな環境値を塗りつぶす
+	//	log.out = // これは存在しない
 		if (! existsSync(out)) {
-			oLog[nm].err = `変換失敗です。出力ファイル ${q.out} が存在しません`;
+			log.err += `変換失敗です。出力ファイル ${log.out} が存在しません`;
 			continue;
 		}
-		oLog[nm].iSize = statSync(inp).size;
-		oLog[nm].oSize = statSync(out).size;
+		log.iSize = statSync(inp).size;
+		log.oSize = statSync(out).size;
 	}
 	log_exit(0);
 });
